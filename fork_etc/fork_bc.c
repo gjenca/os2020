891b13de8143b9727f7e10f3955ced8fe92c3b70
Gejza <gejza@rorschach>
2020-10-06 12:50:03 +0200

    deadlock?

--- fork_etc/fork_bc.c
+++ fork_etc/fork_bc.c
@@ -11,7 +11,10 @@ int main() {
 	pid_t pid;
 	int pipefd_vyr[2];
 	int pipefd_vys[2];
+	int vys;
 	char buf[50];
+	FILE *fpw;
+	FILE *fpr;
 
 	if (pipe(pipefd_vyr)) {
 		perror("pipe");
@@ -27,12 +30,25 @@ int main() {
 		exit(1);
 	}
 	if (pid) {
-		printf("Ja som parent\n");
 		close(pipefd_vys[1]);
 		close(pipefd_vyr[0]);
-		write(pipefd_vyr[1],PRIKLAD,11);
+		fpw=fdopen(pipefd_vyr[1],"w");
+		fpr=fdopen(pipefd_vys[0],"r");
+		//write(pipefd_vyr[1],PRIKLAD,11);
+		for (i=0;i<100;i++) {
+			fprintf(fpw,"9*%d\n",i);
+			fflush(fpw);// az teraz zapise, inak to zostane v buffri FILE objektu
+		}
+		fclose(fpw);
+		for (i=0;i<100;i++) {
+			fscanf(fpr,"%d\n",&vys);
+			printf("9*%d=%d\n",i,vys);
+		}
+		fclose(fpr);
+/*
+		//close(pipefd_vyr[1]);
 		while (1) {
-			n=read(pipefd[0],buf,sizeof(buf)-1);
+			n=read(pipefd_vys[0],buf,sizeof(buf)-1);
 			if (!n) { // end of file (== zapisovy koniec rury je zavrety)
 				break;
 			}
@@ -43,12 +59,21 @@ int main() {
 			// n je pocet bytov precitanych z rury
 			write(1,buf,n);
 		}
+*/
 		
 	}
 	else {
-		printf("Ja som child\n");
-		close(pipefd[0]);
-		write(pipefd[1],"AHOJ\n",5);
+		close(pipefd_vys[0]);
+		close(pipefd_vyr[1]);
+		dup2(pipefd_vyr[0],0);
+		dup2(pipefd_vys[1],1);
+		close(pipefd_vyr[0]);
+		close(pipefd_vys[1]);
+		if (execl("/usr/bin/bc","/usr/bin/bc",NULL)) {
+			perror("execl");
+			exit(-1);
+		}
+		// sem sa nikdy nedostane vypocet
 	}	
 }
 
